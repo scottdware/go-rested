071800e704de83f80f4017ccfc184f083b074fab
Scott Ware <scottdware@gmail.com>
2016-03-02 08:37:11 -0500

    Added function for posting form values

--- rested.go
+++ rested.go
@@ -52,9 +52,8 @@ func (r *Request) BasicAuth(user, password string) {
 	r.Auth = []string{user, password}
 }
 
-// Send issues an HTTP request with the given options. To send/post form values, use a map[string]string
-// type for the body parameter.
-func (r *Request) Send(method, uri string, body interface{}, headers, query map[string]string) *Response {
+// Send issues an HTTP request with the given options.
+func (r *Request) Send(method, uri string, body []byte, headers, query map[string]string) *Response {
 	var req *http.Request
 	var data Response
 
@@ -75,20 +74,71 @@ func (r *Request) Send(method, uri string, body interface{}, headers, query map[
 
 	u.RawQuery = q.Encode()
 
-	switch body.(type) {
-	case []byte:
-		b := bytes.NewReader(body.([]byte))
-		req, _ = http.NewRequest(strings.ToUpper(method), u.String(), b)
-	case map[string]string:
-		form := url.Values{}
-		for k, v := range body.(map[string]string) {
-			form.Add(k, v)
-		}
+	b := bytes.NewReader(body)
+	req, _ = http.NewRequest(strings.ToUpper(method), u.String(), b)
 
-		req, _ = http.NewRequest(strings.ToUpper(method), u.String(), strings.NewReader(form.Encode()))
-		req.PostForm = form
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if len(r.Auth) > 0 {
+		req.SetBasicAuth(r.Auth[0], r.Auth[1])
 	}
+
+	if headers != nil {
+		for k := range headers {
+			req.Header.Add(k, headers[k])
+		}
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		data.Error = err
+
+		return &data
+	}
+
+	defer res.Body.Close()
+
+	payload, _ := ioutil.ReadAll(res.Body)
+	data.Body = payload
+	data.Code = res.StatusCode
+	data.Status = res.Status
+	data.Headers = res.Header
+
+	if res.StatusCode >= 400 {
+		data.Error = fmt.Errorf("HTTP %d: %s", res.StatusCode, string(payload))
+	}
+
+	return &data
+}
+
+// SendForm issues an HTTP POST/PUT, etc. when you need to use form values. The values must be in a map[string]string type.
+func (r *Request) SendForm(method, uri string, form map[string]string, headers, query map[string]string) *Response {
+	var req *http.Request
+	var data Response
+
+	u, err := url.Parse(uri)
+	if err != nil {
+		data.Error = err
+
+		return &data
+	}
+
+	q := u.Query()
+
+	if query != nil {
+		for k := range query {
+			q.Add(k, query[k])
+		}
+	}
+
+	u.RawQuery = q.Encode()
+
+	f := url.Values{}
+	for k, v := range form {
+		f.Add(k, v)
+	}
+
+	req, _ = http.NewRequest(strings.ToUpper(method), u.String(), strings.NewReader(f.Encode()))
+	req.PostForm = f
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	if len(r.Auth) > 0 {
 		req.SetBasicAuth(r.Auth[0], r.Auth[1])
